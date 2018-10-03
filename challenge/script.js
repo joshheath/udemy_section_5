846ddea8d4c43218a49587b2a716334766a4d2c6
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-10-03 13:46:33 +0100

    create questions array and random selection

--- challenge/script.js
+++ challenge/script.js
@@ -7,3 +7,6 @@ var Question = function(question, answers, correct) {
   var q1 = new Question('1. What is 1+2?', ['0. 3', '1. 4'], 0);
   var q2 = new Question('2. What is the capital of England?', ['0. Paris', '1. London'], 1);
   var q3 = new Question('3. Are succulents cool?', ['0. Definitely not', '1. Obviously'], 1);
+
+var questionsArray = [q1, q2, q3];
+var randomQ = questionsArray[Math.floor(Math.random() * questionsArray.length)];
