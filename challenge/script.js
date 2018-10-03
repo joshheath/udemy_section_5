b89ab5d45175a90ae6b1d4097509926b07cf14af
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-10-03 13:48:54 +0100

    wrap code in IIFE

--- challenge/script.js
+++ challenge/script.js
@@ -1,29 +1,32 @@
-var Question = function(question, answers, correct) {
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-  }
 
-  var q1 = new Question('1. What is 1+2?', ['0. 3', '1. 4'], 0);
-  var q2 = new Question('2. What is the capital of England?', ['0. Paris', '1. London'], 1);
-  var q3 = new Question('3. Are succulents cool?', ['0. Definitely not', '1. Obviously'], 1);
+(function() {
+  var Question = function(question, answers, correct) {
+      this.question = question;
+      this.answers = answers;
+      this.correct = correct;
+    }
 
-var questionsArray = [q1, q2, q3];
-var randomQ = questionsArray[Math.floor(Math.random() * questionsArray.length)];
+    var q1 = new Question('1. What is 1+2?', ['0. 3', '1. 4'], 0);
+    var q2 = new Question('2. What is the capital of England?', ['0. Paris', '1. London'], 1);
+    var q3 = new Question('3. Are succulents cool?', ['0. Definitely not', '1. Obviously'], 1);
 
-function AskQuestion(){
-  console.log(randomQ.question);
-  console.log(randomQ.answers);
-};
+  var questionsArray = [q1, q2, q3];
+  var randomQ = questionsArray[Math.floor(Math.random() * questionsArray.length)];
 
-function AnswerInput(){
-  var ask = parseInt(prompt('Please enter the number of your answer'));
-  if(ask === randomQ.correct){
-    console.log('Congrats - that is correct');
-  } else {
-    console.log('Sorry, that is wrong');
-  }
-};
+  function AskQuestion(){
+    console.log(randomQ.question);
+    console.log(randomQ.answers);
+  };
 
-AskQuestion();
-AnswerInput();
+  function AnswerInput(){
+    var ask = parseInt(prompt('Please enter the number of your answer'));
+    if(ask === randomQ.correct){
+      console.log('Congrats - that is correct');
+    } else {
+      console.log('Sorry, that is wrong');
+    }
+  };
+
+  AskQuestion();
+  AnswerInput();
+}());
