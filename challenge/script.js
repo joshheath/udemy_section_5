96e2a6d91c0bad383f717df25a1ee34a716c3950
Josh Heath <37807868+joshheath@users.noreply.github.com>
2018-10-03 13:45:47 +0100

    add three test questions

--- challenge/script.js
+++ challenge/script.js
@@ -3,3 +3,7 @@ var Question = function(question, answers, correct) {
     this.answers = answers;
     this.correct = correct;
   }
+
+  var q1 = new Question('1. What is 1+2?', ['0. 3', '1. 4'], 0);
+  var q2 = new Question('2. What is the capital of England?', ['0. Paris', '1. London'], 1);
+  var q3 = new Question('3. Are succulents cool?', ['0. Definitely not', '1. Obviously'], 1);
